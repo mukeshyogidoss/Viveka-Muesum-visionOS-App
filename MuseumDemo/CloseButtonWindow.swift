6604888c9324ddcb45fdecd4371d8e8e7ec58dc1
mukeshyogidoss <mukeshyogidoss@gmail.com>
2026-01-08 13:35:52 +0530

    New Window Added with Close Button

---
+++ MuseumDemo/CloseButtonWindow.swift
@@ -0,0 +1,18 @@
+//
+//  CloseButtonWindow.swift
+//  MuseumDemo
+//
+//  Created by Intern on 08/01/26.
+//
+
+import SwiftUI
+
+struct CloseButtonWindow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloseButtonWindow()
+}
---
+++ MuseumDemo/isImmersiveOpen.swift
@@ -0,0 +1,18 @@
+//
+//  isImmersiveOpen.swift
+//  MuseumDemo
+//
+//  Created by Intern on 08/01/26.
+//
+
+import SwiftUI
+
+struct isImmersiveOpen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    isImmersiveOpen()
+}
